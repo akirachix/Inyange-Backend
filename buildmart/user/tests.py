1b8b731ca818e216ada21b94aed72463b41be12f
rehema_funani <rehemafunani@gmail.com>
2024-09-19 13:23:05 +0300

    adds files

--- buildmart/user/tests.py
+++ buildmart/user/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-
 class UserManagerTest(TestCase):
     def setUp(self):
         self.email = "user@example.com"
