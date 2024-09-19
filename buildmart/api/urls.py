6eb498259a0d2c297386762f8aa1d0bc0d1f3abf
rehema_funani <rehemafunani@gmail.com>
2024-09-19 13:52:30 +0300

    adds files

--- buildmart/api/urls.py
+++ buildmart/api/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 
 from .views import MaterialDetailView
 from .views import MaterialListView
+from .views import UserListView
+from .views import LoginListView
+from .views import RegisterView
 
 urlpatterns = [
     path("materials/", MaterialListView.as_view(), name = "material_list_view"),
     path("material/<int:id>/", MaterialDetailView.as_view(), name="material_detail_view"),
+    path("user/", UserListView.as_view(), name="user_list_view"),
+    path("login/", LoginListView.as_view(), name="login_list_view"),
+    path("register/", RegisterView.as_view(), name="register_list_view"),
+
+
 ]
--- buildmart/buildmart/urls.py
+++ buildmart/buildmart/urls.py
@@ -20,4 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path("material/", include('material.urls')),
+    path("user/", include('user.urls')),
+    path("auth/", include('accounts.urls')),
+
 ]
