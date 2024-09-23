287003165aac10fa4a2682170c04148d71e34976
student <faithnyamburawamui@gmail.com>
2024-09-23 16:56:11 +0300

    did unit test

--- buildmart/api/serializers.py
+++ buildmart/api/serializers.py
@@ -23,6 +23,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password', 'email', 'phone_number', 'first_name', 'last_name','user_role')
+        extra_kwargs = {'password':{'write_only': True}}
      
     def validate(self, data):
         if User.objects.filter(username=data['username']).exists():
